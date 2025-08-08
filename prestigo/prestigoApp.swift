cbd1a1893bf06a772bb45a000e6013b10b66e019
berk üçkardeş <huckardes21@ku.edu.tr>
2025-08-08 13:42:20 +0300

    Initial Commit

---
+++ prestigo/prestigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  prestigoApp.swift
+//  prestigo
+//
+//  Created by Berk  on 8.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct prestigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ prestigoTests/prestigoTests.swift
@@ -0,0 +1,16 @@
+//
+//  prestigoTests.swift
+//  prestigoTests
+//
+//  Created by Berk  on 8.08.2025.
+//
+
+import Testing
+
+struct prestigoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
